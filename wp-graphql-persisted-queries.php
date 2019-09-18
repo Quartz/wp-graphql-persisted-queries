702ed606c2a57c50cd86b5f60a224555cd5fb198
chriszarate <chris@zarate.org>
2019-09-17 23:52:05 -0400

    Update with improvements

--- wp-graphql-persisted-queries.php
+++ wp-graphql-persisted-queries.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/Quartz/wp-graphql-persisted-queries
  * Description: Provides persistence to GraphQL queries allowing them to be queried by ID or hash. Requires WPGraphQL 0.2.0 or newer.
  * Author: Chris Zarate, Quartz
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author URI: https://qz.com/
  *
  * @package wp-graphql-persisted-queries
@@ -14,4 +14,5 @@ namespace WPGraphQL\Extensions\PersistedQueries;
 
 require_once( __DIR__ . '/src/loader.php' );
 
-add_action( 'graphql_init', array( new Loader(), 'init' ), 10, 0 );
+// Higher priority allows other code at default priority to run first.
+add_action( 'graphql_init', array( new Loader(), 'init' ), 20, 0 );
